cd2548406c7fdd5eceec3eb6c89db12f8ccef687
agvera <gienovera@gmail.com>
2018-12-04 00:49:46 +0800

    Added Token on Signup

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,50 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
+const jwt = require('jsonwebtoken')
 
-let User = mongoose.model('User',{
+const UserSchema = mongoose.Schema({
     email: {
         type: String,
         required: true,
         minlength: 1,
-        trim: true
-    }
+        trim: true,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email address'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
+    
 })
+
+UserSchema.methods.generateAuthToken = function() {
+    const user = this
+    const access = 'auth'
+    const jwt = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString()
+
+    // user.tokens.push({access,token})
+    user.tokens = user.tokens.concat([{access,token}])
+    
+    return user.save().then(() => {
+        return token
+    })
+}
+
+const User = mongoose.model('User', UserSchema)
 
 module.exports = {User}
